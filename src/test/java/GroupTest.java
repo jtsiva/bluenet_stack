f047d826563d00398e5a9471930607a925852a5c
Josh Siva <jtsiva@gmail.com>
2018-05-11 16:58:58 -0400

    added test files

---
+++ src/test/java/GroupTest.java
@@ -0,0 +1,17 @@
+package  nd.edu.bluenet_stack;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.nio.ByteBuffer;
+import java.util.*;
+
+
+public class GroupTest {
+	private final static String MY_ID = "1111";
+	private AdvertisementPayload mAdvPayload = null;
+
+	@Before
+	public void setup() {
+	}
+}
