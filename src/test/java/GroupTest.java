1518334eee792b09d76927f3e6881a2a16cd8928
Josh Siva <jtsiva@gmail.com>
2018-05-15 18:31:07 -0400

    named and geo group tests done

--- src/test/java/GroupTest.java
+++ src/test/java/GroupTest.java
@@ -42,4 +42,56 @@ public class GroupTest {
 		grp.leave();
 		assertTrue(!grp.getStatus());
 	}
+
+	//Named group
+
+	@Test
+	public void shouldSetupMinimalNamedGroup() {
+		NamedGroup grp = new NamedGroup(MY_ID);
+		assertEquals(MY_ID, new String(grp.getID()));
+		assertEquals(Group.NAMED_GROUP, grp.getType());
+	}
+
+	@Test
+	public void shouldSetupNamedGroup() {
+		NamedGroup grp = new NamedGroup(MY_ID, "awesome_group");
+		assertEquals(MY_ID, new String(grp.getID()));
+		assertEquals(Group.NAMED_GROUP, grp.getType());
+		assertEquals("awesome_group", grp.getName());
+	}
+
+	// Geo group
+
+	public void shouldSetupMinimalGeoGroup() {
+		GeoGroup grp = new GeoGroup(MY_ID);
+		assertEquals(MY_ID, new String(grp.getID()));
+		assertEquals(Group.GEO_GROUP, grp.getType());
+		assertEquals(0.0, grp.getLatitude(), .00001);
+		assertEquals(0.0, grp.getLongitude(), .00001);
+		assertEquals(0.0, grp.getRadius(), .00001);
+	}
+
+	@Test
+	public void shouldSetupGeoGroup() {
+		GeoGroup grp = new GeoGroup(MY_ID, 10.005f,-35.12345f,15f);
+		assertEquals(MY_ID, new String(grp.getID()));
+		assertEquals(Group.GEO_GROUP, grp.getType());
+		assertEquals(10.005, grp.getLatitude(), .00001);
+		assertEquals(-35.12345, grp.getLongitude(), .00001);
+		assertEquals(15.0, grp.getRadius(), .00001);
+	}
+
+	@Test
+	public void shouldJoinGeoGroup() {
+		GeoGroup grp = new GeoGroup(MY_ID, 10.005f,-35.12345f,15f);
+		grp.join(10.005f,-35.12354f);
+		assertTrue(grp.getStatus());
+	}
+
+	@Test
+	public void shouldNotJoinGeoGroup() {
+		GeoGroup grp = new GeoGroup(MY_ID, 10.005f,-35.12345f,15f);
+		grp.join(11f, -35.12345f);
+		assertTrue(!grp.getStatus());
+	}
 }
