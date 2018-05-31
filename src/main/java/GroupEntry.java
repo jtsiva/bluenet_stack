5203751a3bf7bfc93aac48a4c85f53e7a5a72d09
Josh Siva <jtsiva@gmail.com>
2018-05-31 15:33:13 -0400

    Groupentry comments done

--- src/main/java/GroupEntry.java
+++ src/main/java/GroupEntry.java
@@ -3,19 +3,41 @@ package nd.edu.bluenet_stack;
 import java.util.*;
 import java.sql.Timestamp;
 
+/**
+ * This class captures group information as well as when that group has been
+ * updated. Useful for tracking the staleness of a group
+ * 
+ * @author Josh Siva
+ * @see Group
+ */
 public class GroupEntry {
 	public Group mGroup;
 	public Timestamp mTimestamp;
 
+	/**
+	 * set the group and timestamp to null by default
+	 */
 	public GroupEntry() {
 		mGroup = null;
 		mTimestamp = null;
 	}
 
+	/**
+	 * Initialize the group and set the timestamp
+	 * 
+	 * @param grp the group used to initialize the entry
+	 */
 	public GroupEntry(Group grp) {
 		update(grp);
 	}
 
+	/**
+	 * Check whether the groups are equal
+	 * 
+	 * @param obj the group or object to check for equality
+	 * @return true if the groups are equal, false otherwise
+	 * @see Group
+	 */
 	@Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -38,11 +60,19 @@ public class GroupEntry {
         return true;
     }
 
+    /**
+     * Update the group in the entry and update the timestamp to now
+     * 
+     * @param grp new group to which the group in this entry is set
+     */
 	public void update(Group grp) {
 		mGroup = grp;
 		mTimestamp = new Timestamp(System.currentTimeMillis());
 	}
 
+	/**
+	 * Update the timestamp for the group entry to now.
+	 */
 	public void update() {
 		mTimestamp = new Timestamp(System.currentTimeMillis());
 	}
