94a430e91b818ae7dc004400251c707cc7d31522
Josh Siva <jtsiva@gmail.com>
2018-05-31 13:53:48 -0400

    Coordinate comments done

--- src/main/java/Coordinate.java
+++ src/main/java/Coordinate.java
@@ -1,14 +1,27 @@
 package nd.edu.bluenet_stack;
 
+/**
+ * Object that nicely encapsulates latitude and longitude together
+ * 
+ * @author Josh siva
+ */
 public class Coordinate {
 	public float mLatitude;
 	public float mLongitude;
 
+	/**
+	 * @return Coordinate object with latitude and longitude initialized to (0.0,0.0)
+	 */
 	public Coordinate() {
 		mLatitude = 0.0f;
 		mLongitude = 0.0f;
 	}
 
+	/**
+	 * @param lat latitude to set
+	 * @param lon longitude to set
+	 * @return Coordinate object with latitude and longitude initialized to passed in values
+	 */
 	public Coordinate(float lat, float lon) {
 		mLatitude = lat;
 		mLongitude = lon;
