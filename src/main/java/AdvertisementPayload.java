1f1833520ea22528c7135da3a4d162f67fd9ccd8
Josh Siva <jtsiva@gmail.com>
2018-05-31 13:42:40 -0400

    completed documentation on AdvertisementPayload and BlueNetIFace

--- src/main/java/AdvertisementPayload.java
+++ src/main/java/AdvertisementPayload.java
@@ -4,8 +4,22 @@ import java.util.Arrays;
 import java.nio.charset.StandardCharsets;
 
 /**
- * Created by jerry on 4/9/18.
- * updated by josh on 4/16/18
+ * This class encapsulates both the header and message elements required for
+ * communication. The class identifies different message types, parses byte
+ * strings to set this classes variables, and dumps the classes variables into
+ * a byte array.
+ *
+ * <p>The expected and produced format of the byte array is the following
+ * <pre>{@code
+ *       srcID || destID || msgID || ttl | hp | unused || len || msg 
+ *       4B       4B        1B       3b    1b   4b        1B     9B
+ * }</pre>
+ *
+ * <p>This class provides mechanisms for setting priority, tracking next hop
+ * neighbors, and supporting both push and pull based communication styles. 
+ *
+ * @author  Josh Siva
+ * @see     MessageRetriever
  */
 
 public class AdvertisementPayload {
@@ -19,8 +33,6 @@ public class AdvertisementPayload {
 
     public final static byte MAX_TTL = 0b111;
 
-    // srcID || destID || msgID || ttl | hp | unused || len || msg 
-    // 4B       4B        1B       3b    1b   4b        1B     9B
 
     private byte[] srcID = null;
     private byte[] destID = null;
@@ -36,6 +48,15 @@ public class AdvertisementPayload {
     private MessageRetriever msgRetriever = null;
     private boolean needRetriever = false;
 
+    /**
+     * Override the equals operator so that payloads can be compared for
+     * equality. This is useful for determining whether we have seen a
+     * message before. For two AdvertisementPayloads to be equal, both the
+     * msgID and msgSrc must be equal.
+     * 
+     * @param  obj the object against which we are comparing
+     * @return true if the AdvertisementPayloads are equal, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -63,6 +84,16 @@ public class AdvertisementPayload {
         return true;
     }
 
+    /**
+     * Parse a byte array into the appropriate member variables. The byte array
+     * is assumed to be properly formatted. If the length of the byte array is
+     * greater than the length of the header then it is assumed that the message
+     * was pushed along with the header; otherwise, we will need to retrieve the
+     * message through a callback
+     * 
+     * @param bytes the byte array that we parse
+     * @see MessageRetriever
+     */
     public void fromBytes(byte[] bytes) {
         byte[] srcID = Arrays.copyOfRange(bytes, 0, 4);
         byte[] destID = Arrays.copyOfRange(bytes, 4, 8);
@@ -89,10 +120,24 @@ public class AdvertisementPayload {
 
     }
 
+    /**
+     * Set the java equivalent of a callback function for getting the message
+     * byte array at some later point in time. The byte array will be retrieved
+     * from a separate characteristic in the appropriate GATT service.
+     * 
+     * @param retriever the implemented interface that is used to read the
+     *        message
+     * @see MessageRetriever
+     */
     public void setRetriever(MessageRetriever retriever) {
         msgRetriever = retriever;
     }
 
+    /**
+     *  Return the header information as a byte array
+     * 
+     * @return the bytes that constitute the header
+     */
     public byte[] getHeader(){
         byte [] bytes = new byte[11];
         System.arraycopy(srcID, 0, bytes,0,srcID.length);
@@ -108,6 +153,11 @@ public class AdvertisementPayload {
         return bytes;
     }
 
+    /**
+     * A convenience function for printing the header in hex. For debugging.
+     * 
+     * @return the header in hex as a char array
+     */
     public char[] getPrettyBytes() {
         final  char[] hexArray = "0123456789ABCDEF".toCharArray();
         byte[] bytes = this.getHeader();
@@ -121,66 +171,126 @@ public class AdvertisementPayload {
         return hexChars;
     }
 
+    /**
+     * @param srcID the alphanumeric BlueNet ID string for the sender
+     */
     public void setSrcID(String srcID){
         this.srcID = srcID.getBytes(StandardCharsets.UTF_8);
     }
 
+    /**
+     * @param srcID the alphanumeric BlueNet ID for the sender as a byte array
+     */
     public void setSrcID(byte [] srcID){
         this.srcID = srcID.clone();
     }
 
+    /**
+     * @return the byte array that is the BlueNet ID for the sender if set,
+     *         else null
+     */
     public byte[] getSrcID() {
         return srcID;
     }
 
+    /**
+     * @param destID the alphanumeric BlueNet ID for the receiver as a String
+     */
     public void setDestID(String destID){
         this.destID = destID.getBytes(StandardCharsets.UTF_8);
     }
 
+    /**
+    * @param destID the alphanumeric BlueNet ID for the receiver as a byte 
+    *               array
+    */
     public void setDestID(byte [] destID){
         this.destID = destID.clone();
     }
 
+    /**
+     * @return the byte array that is the BlueNet ID for the receiver if set,
+     *         else null
+     */
     public byte[] getDestID() {
         return destID;
     }
 
+    /**
+     * Since the one hop neighbor is not always the source of the message, we
+     * want to keep track of the direct neighbor who sent the message. The
+     * one hop neighbor can be determined by checking the TTL.
+     * 
+     * @param neighborID the alphanumeric BlueNetID for the one hop neighbor 
+     *                   that sent this message as a String
+     */
     public void setOneHopNeighbor(String neighborID) {
         this.oneHopNeighbor = neighborID.getBytes(StandardCharsets.UTF_8);
     }
 
+    /**
+     * @return the alphanumeric BlueNet ID of the one hop neighbor that sent
+     *         the message as a byte array if set, else null
+     */
     public byte[] getOneHopNeighbor () {
         return oneHopNeighbor;
     }
 
+    /**
+     * @param msgID 1 byte number that identifies the message
+     */
     public void setMsgID (byte msgID) {
         this.msgID = msgID;
     }
 
+    /**
+     * @return the ID number of the message if set, else 0
+     */
     public byte getMsgID () {
         return msgID;
     }
 
+    /**
+     * @param msgType 16-bit UUID corresponding to the GATT service ID. NOT
+     *                included in BlueNet header
+     */
     public void setMsgType (int msgType) {
         this.msgType = msgType;
     }
-
+    
+    /**
+     * @return the message type if set, 0 otherwise
+     */
     public int getMsgType () {
         return msgType;
     }
 
+    /**
+     * @return the current time-to-live (by hop) of this message, MAX_TTL by
+     *         default.
+     */
     public byte getTTL() {
         return this.ttl;
     }
 
+    /**
+     * Decrease the TTL by 1
+     */
     public void decTTL () {
         this.ttl -= 1;
     }
 
+    /**
+     * @return true if the high priority bit has been set, false otherwise
+     */
     public boolean isHighPriority() {
         return (1 == this.hp);
     }
 
+    /**
+     * @param priority true or false depending on whether we want the message
+     *        to be marked as high priority
+     */
     public void setHighPriority(boolean priority) {
         if (priority) {
             this.hp = 0b1;
@@ -190,15 +300,31 @@ public class AdvertisementPayload {
         }
     }
 
+    /**
+     * @return the length of the message as indicated in the header
+     */
     public byte getMsgLength() {
         return this.len;
     }
 
+    /**
+     * Set the message for the payload as well as the length field in the
+     * header
+     * 
+     * @param msg the byte array containing the message
+     */
     public void setMsg(byte[] msg) {
         this.len = (byte)msg.length;
         this.msg = msg.clone();
     }
 
+    /**
+     * The message is retrieved either from the member variable or by using
+     * a callback to pull in the message.
+     * 
+     * @return the byte array constituting the message
+     * @see MessageRetriever
+     */
     public byte[] getMsg() {
         if (needRetriever) {
             msg = msgRetriever.retrieve();// we need to go pull in the message
--- src/main/java/BlueNetIFace.java
+++ src/main/java/BlueNetIFace.java
@@ -1,22 +1,98 @@
 package nd.edu.bluenet_stack;
 
-
+/**
+ * Application-facing interface for the BlueNet stack
+ * 
+ * @author Josh Siva
+ */
 public interface BlueNetIFace {
-   public String getMyID(); // Returns id of user's device
-   public int write(String destID, String input); // Returns number of bytes written, error code (-1) otherwise
+   /**
+    * @return the BlueNet ID of this device as a String
+    */
+   public String getMyID();
+
+   /**
+    * Write a message/payload to BlueNet network
+    * 
+    * @param destID the alphanumeric BlueNet ID of the intended recipient
+    * @param input the message to be sent
+    * @return the number of bytes written or -1 in the case of an error
+    */
+   public int write(String destID, String input);
 
    //Unsure about the inclusion this version of a write
    //public int write(String destID, String input, boolean highPriority); //set whether this message is high priority
    
+   /**
+    * @param resultHandler the user-implemented callback so that reads can be
+    *        be delivered asynchronously
+    * @see Result
+    */
    public void regCallback(Result resultHandler); // sets handler to be called on message received
-   public String[] getNeighbors(); // returns array of ids connected to this device
+   
+   /**
+    * @return array of BlueNet IDs of all discovered devices in the network
+    * @see LocationManager
+    */
+   public String[] getNeighbors(); 
+   
+   /**
+    * @param id alphanumeric BlueNet ID of device whose location you want
+    * @return the average location of the device (as lat lon) or 0.0 0.0 if location not known
+    * @see LocationManager
+    */
    public String getLocation(String id); // returns average location of id (as: lat lon), or 0.0 0.0 if id does not exist
 
    //Group operations
+
+   /**
+    * @return Group objects for all groups (including those that are not joinable)
+    * @see Group
+    * @see GeoGroup
+    * @see NamedGroup
+    * @see GroupManager
+    */
    public Group [] getGroups();//return joinable groups (maybe not geo since those are automatic)
+   
+   /**
+    * Create a new named group
+    * 
+    * @param name to be applied to the group
+    * @see Group
+    * @see NamedGroup
+    */
    public void addGroup(String name); //add named group
-   public void addGroup(float lat, float lon, float rad); //add geographic group
-   public boolean joinGroup(String id); //join group with id; returns true if joined, false otherwise
+   
+   /**
+    * Create a new geographical group
+    * 
+    * @param lat latitude of center of group location
+    * @param lon longitude of center of group location
+    * @param rad radius from center of group location that is considered "in" 
+    *            the group
+    * @see Group
+    * @see GeoGroup
+    */
+   public void addGroup(float lat, float lon, float rad);
+   
+   /**
+    * Attempt to join a group. Some groups, like geographical groups, cannot be
+    * joined in the way. Entry into a group means that messages addressed to
+    * that group will be able to be received by this node
+    * 
+    * @param id alphanumeric BlueNet ID of group that user wants to join
+    * @return true if the group was joined, false otherwise
+    */
+   public boolean joinGroup(String id);
+
+   /**
+    * Attempt to leave a group. Some groups cannot be left in this way.
+    * Departure from a group means that messages addressed to that group will not
+    * be received by this node
+    * 
+    * @param id alphanumeric BlueNet ID of group that user wants to leave
+    * @return true if the group was left, false otherwise
+    */
    public boolean leaveGroup(String id); //leave group with id; returns true if left, false otherwise
    		
 
