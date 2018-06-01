c4f7b420ccf0a213269bb1db56bb286d66512f80
Josh Siva <jtsiva@gmail.com>
2018-06-01 09:31:01 -0400

    MessageLayer comments done

--- src/main/java/MessageLayer.java
+++ src/main/java/MessageLayer.java
@@ -2,6 +2,15 @@ package nd.edu.bluenet_stack;
 
 import java.util.*;
 
+/**
+ * This class handles simple message filtering and helps provide
+ * the final few fields to the payload before it is sent
+ * 
+ * @author Josh Siva
+ * @see LayerIFace
+ * @see Reader
+ * @see Writer
+ */
 public class MessageLayer implements LayerIFace {
 	private final static int SMALL_MSG_MAX = 9;
 	protected Reader mReadCB;
@@ -12,6 +21,25 @@ public class MessageLayer implements LayerIFace {
 	public int filterWindowSize = 126;
 	private ArrayList<AdvertisementPayload> mWindow = new ArrayList<>(); 
 
+	/**
+	 * Default constructor. Set msg ID (to be assigned to outgoing messages)
+	 * to 0. This will be incremented for each new outgoing message.
+	 */
+	public MessageLayer() {
+		mReadCB = null;
+		mWriteCB = null;
+		mMsgIndex = 0b0;
+	}
+
+	/**
+	 * Check to see whether the current message is a "new" message based on
+	 * source ID and the message ID
+	 * 
+	 * @param advPayload the payload
+	 * @return true if the message has not been repeated within the window,
+	 * 		   false otherwise.
+	 * @see AdvertisementPayload
+	 */
 	private boolean isNew(AdvertisementPayload advPayload) {
 		if (!mWindow.contains(advPayload)) {
 			mWindow.add(advPayload); //haven't seen this message so put in list
@@ -25,11 +53,6 @@ public class MessageLayer implements LayerIFace {
 		return false;
 	}
 
-	public MessageLayer() {
-		mReadCB = null;
-		mWriteCB = null;
-		mMsgIndex = 0b0;
-	}
 
 	public void setReadCB (Reader reader) {
 		mReadCB = reader;
@@ -43,16 +66,15 @@ public class MessageLayer implements LayerIFace {
 		mQueryCB = q;
 	}
 
-	/*
-	* Only meant to be called with COMPLETE payloads. That is, data (as bytes)
-	* should constitute a complete message from BLE either through GATT or SC
-	*/
+
+	/**
+	 * Check to make sure we haven't already seen the message.
+	 * 
+	 * @param advPayload a complete payload from a lower layer
+	 * @return result of read callback-- 0 if successful, negative otherwise
+	 */
 	public int read(AdvertisementPayload advPayload) {
-		/*
-			Check to make sure we haven't already seen the message
-			Double check that message type is correct (only pass up small/reg messages)
-			handle message sequencing?
-		*/
+
 		//System.out.println("ML hit");
 		if (isNew(advPayload)) { //no repeats!
 
@@ -68,9 +90,18 @@ public class MessageLayer implements LayerIFace {
 			//}
 		}
 		
-		return 0;
+		return 0; //even though we're ignoring the message, it's not an error so return 0
 	}
 
+	/**
+	 * Take a simple byte message and a destination and create an complete
+	 * AdvertisementPayload to be passed down to a lower layer of the stack.
+	 * Set the message type based on the size of the message array
+	 * 
+	 * @param dest the alphanumeric BlueNet ID of the destination node
+	 * @param message the stuff to send
+	 * @return the result of writing, 0 if successful, negative otherwise
+	 */
 	public int write(String dest, byte[] message) {
 		/*
 			Set up the fields for the advertisement:
@@ -99,13 +130,28 @@ public class MessageLayer implements LayerIFace {
 		return this.write(advPayload);
 	}
 
+	/**
+	 * @param src
+	 * @param message
+	 * @throws UnsupportedOperationException
+	 */
 	public int read(String src, byte[] message) {
 		throw new java.lang.UnsupportedOperationException("Not supported.");
 	}
+
+	/**
+	 * If we receive an AdvertisementPayload, it's safe to assume that all 
+	 * necessary fields have been filled out except for the msgID which can 
+	 * only be set here.
+	 * 
+	 * <p>However, we do not want to change the msgID of a forwarded message,
+	 * so just pass it through
+	 * 
+	 * @param advPayload the payload to write to a lower layer of the stack
+	 * @return result of write--0 if successful, negative otherwise
+	 */
 	public int write(AdvertisementPayload advPayload) {
-		// If we receive an AdvertisementPayload, it's safe to assume that all necessary
-		// fields have been filled out except for the msgID which can only be set here.
-		// However, we do not want to change the msgID of a forwarded message!
+		// 
 
 		if (Objects.equals(mQueryCB.ask("global.id"), new String(advPayload.getSrcID()))) {
 			advPayload.setMsgID(mMsgIndex);
@@ -115,6 +161,12 @@ public class MessageLayer implements LayerIFace {
 		return mWriteCB.write(advPayload);
 	}
 
+	/**
+	 * Respond to queries. Only handles identification of layer
+	 * 
+	 * @param myQuery the query
+	 * @return response to query or empty String if not handled
+	 */
 	public String query(String myQuery) {
 		String resultString = new String();
 		if (Objects.equals(myQuery, "tag")) {
