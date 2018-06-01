813b8936d82ff1a542da4edeca12558811fb87b7
Josh Siva <jtsiva@gmail.com>
2018-06-01 09:38:39 -0400

    MessageRetriever comments done

--- src/main/java/MessageRetriever.java
+++ src/main/java/MessageRetriever.java
@@ -1,5 +1,17 @@
 package nd.edu.bluenet_stack;
 
+/**
+ * Callback mechanism for handling pull based communication. The retrieve
+ * function should read the characteristic containing the message and return
+ * it through this function.
+ * 
+ * @author Josh Siva
+ * @see AdvertisementPayload
+ */
 public interface MessageRetriever {
+
+	/**
+	 * @return the contents read from the message characteristic
+	 */
 	public byte[] retrieve();
 }
